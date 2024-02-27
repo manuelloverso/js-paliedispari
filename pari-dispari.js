ce4a759f10b5282c9bf7d44ecf4c87b7433511dd
manuelloverso <manuelloverso@gmail.com>
2024-02-27 15:16:23 +0100

    add even or odd exercise

--- pari-dispari.js
+++ pari-dispari.js
@@ -1,20 +1,30 @@
 // Variable Declaration
-const evenOrOdd = "dispari";
-const usernumb = 5;
-console.log(usernumb);
+const evenOrOddChoice = prompt("Scrivi 'pari' o 'dispari' ");
+const userNumb = Number(prompt("Inserisci un numero da 1 a 5"));
+console.log(`User : ${userNumb}`);
 const computerNumb = Math.floor(Math.random() * 5 + 1);
-console.log(computerNumb);
+console.log(`Computer : ${computerNumb}`);
 
-// Sum the two numbers
-const sum = usernumb + computerNumb;
+function evenOrOdd(userChoice, userNumb, computerNumb) {
+  // Sum the two numbers
+  const sum = userNumb + computerNumb;
 
-// Conditions to check who wins
-if (sum % 2 == 0 && evenOrOdd == "pari") {
-  console.log("You win");
-} else if (sum % 2 == 0 && evenOrOdd == "dispari") {
-  console.log("You lose");
-} else if (sum % 2 != 0 && evenOrOdd == "pari") {
-  console.log("You lose");
-} else if (sum % 2 != 0 && evenOrOdd == "dispari") {
-  console.log("You win");
+  let playerWins = false;
+
+  // Conditions to check who wins
+  if (sum % 2 == 0 && userChoice == "pari") {
+    //console.log("You win");
+    playerWins = true;
+  } else if (sum % 2 != 0 && userChoice == "dispari") {
+    //console.log("You win");
+    playerWins = true;
+  }
+
+  return playerWins;
+}
+
+if (evenOrOdd(evenOrOddChoice, userNumb, computerNumb)) {
+  alert("Hai Vinto");
+} else {
+  alert("Hai Perso");
 }
