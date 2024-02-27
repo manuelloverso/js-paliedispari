6a0763d5548fbf5443306b1584404ffeaf8664f3
manuelloverso <manuelloverso@gmail.com>
2024-02-27 14:35:43 +0100

    addd function for palindrome

--- palindromo.js
+++ palindromo.js
@@ -2,18 +2,26 @@
 //const word = prompt("Inserisci una parola per verificare se è palindroma");
 //const word = "ciao";
 
-let word = "ciao";
+const word = prompt("Inserisci una parola per verificare se sia palindroma");
 
-//Split the stringo into an array
-let wordSplit = word.split("");
-console.log(wordSplit);
+function palindromeCheck(word) {
+  //Split the stringo into an array
+  const wordSplit = word.split("");
+  console.log(wordSplit);
 
-//Reverse the array
-wordSplit.reverse();
-console.log(wordSplit);
+  //Reverse the array
+  wordSplit.reverse();
+  console.log(wordSplit);
 
-//Rejoin the array into a string
-const reversedWord = wordSplit.join("");
-console.log(reversedWord);
+  //Rejoin the array into a string
+  const reversedWord = wordSplit.join("");
+  console.log(reversedWord);
 
-//palindromeCheck();
+  if (word == reversedWord) {
+    console.log("La parola inserita è palindroma");
+  } else {
+    console.log("La parola inserita NON è palindroma");
+  }
+}
+
+palindromeCheck(word);
