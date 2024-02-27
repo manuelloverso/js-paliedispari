89e753fde4b7c94d578fb217f7182631ace456ed
manuelloverso <manuelloverso@gmail.com>
2024-02-27 14:39:00 +0100

    add full palindrome exercise

--- palindromo.js
+++ palindromo.js
@@ -7,20 +7,20 @@ const word = prompt("Inserisci una parola per verificare se sia palindroma");
 function palindromeCheck(word) {
   //Split the stringo into an array
   const wordSplit = word.split("");
-  console.log(wordSplit);
+  //console.log(wordSplit);
 
   //Reverse the array
   wordSplit.reverse();
-  console.log(wordSplit);
+  //console.log(wordSplit);
 
   //Rejoin the array into a string
   const reversedWord = wordSplit.join("");
   console.log(reversedWord);
 
   if (word == reversedWord) {
-    console.log("La parola inserita è palindroma");
+    return alert("la parola inserita è palindroma");
   } else {
-    console.log("La parola inserita NON è palindroma");
+    return alert("La parola inserita NON è palindroma");
   }
 }
 
